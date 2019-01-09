dd71520611feabd28a3bffceb8a372a158f5bbc3
ifyouwanagoa <42785783+ifyouwanagoa@users.noreply.github.com>
2019-01-09 12:00:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,97 +6,28 @@ const client = new Discord.Client();
 client.on('ready', () => {                           
 });
 
-client.on('message', message => {
-	var prefix = "$";
-   if(!message.channel.guild) return;
-if(message.content.startsWith(prefix + 'clear')) {
-if(!message.channel.guild) return message.channel.send('**This Command is Just For Servers**').then(m => m.delete(5000));
-if(!message.member.hasPermission('MANAGE_MESSAGES')) return      message.channel.send('**You Do not have permission** `MANAGE_MESSAGES`' );
-let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-let request = `Requested By ${message.author.username}`;
-message.channel.send(`**Are You sure you want to clear the chat?**`).then(msg => {
-msg.react('✅')
-.then(() => msg.react('❌'))
-.then(() =>msg.react('✅'))
-
-let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-
-let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
-let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
-reaction1.on("collect", r => {
-message.channel.send(`Chat will delete`).then(m => m.delete(5000));
-var msg;
-        msg = parseInt();
-
-      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-      message.channel.sendMessage("", {embed: {
-        title: "`` Chat Deleted ``",
-        color: 0x06DF00,
-        footer: {
-
-        }
-      }}).then(msg => {msg.delete(3000)});
-
-})
-reaction2.on("collect", r => {
-message.channel.send(`**Chat deletion cancelled**`).then(m => m.delete(5000));
-msg.delete();
-})
-})
-}
-});
-
-
-
-
-
-
-
- 
-client.on('message', message => {
-          var prefix = "$";
-    if(message.content.startsWith(prefix + 'rep')) {//////////////////- Mál ,ϻά𝔥𝔪𝔬𝔲𝓓-QuaStyle≽ܫ≼ 5661
-      if(!message.channel.guild) return;
-                    moment.locale('en');
-                  var getvalueof = message.mentions.users.first()
-                    if(!getvalueof) return message.channel.send(`**:mag: |  ${message.author.username}, the user could not be found.    **`);
-                       if(getvalueof.id == message.author.id) return message.channel.send(`**${message.author.username}, you cant give yourself a reputation !**`)
-    if(profile[message.author.id].reps != moment().format('L')) {
-            profile[message.author.id].reps = moment().format('L');
-            profile[getvalueof.id].rep = Math.floor(profile[getvalueof.id].rep+1);
-         message.channel.send(`** :up:  |  ${message.author.username} has given ${getvalueof} a reputation point!**`)
-        } else {
-         message.channel.send(`**:stopwatch: |  ${message.author.username}, you can raward more reputation  ${moment().endOf('day').fromNow()} **`)
-        }
-       }
-       fs.writeFile('profile.json', JSON.stringify(profile), (err) => {
-if (err) console.error(err);
-})
-});//////////////////- Mál ,ϻά𝔥𝔪𝔬𝔲𝓓-QuaStyle≽ܫ≼ 5661
- 
 client.on("message", (message) => {
   let men = message.mentions.users.first()
  
   if (message.author.bot) return;
     if (message.author.id === client.user.id) return;
-    if(!message.channel.guild) return;
+    if(!message.channel.guild) return;/// Mal , Codes
 if (message.content.startsWith(prefix + 'credit')) {
   if(men) {
     if (!profile[men.id]) profile[men.id] = {
     lastDaily:'Not Collected',
     credits: 1,
   };
-  }
+  } /// Mal , Codes
   if(men) {
-message.channel.send(`** ${men.username}, :credit_card: balance` + " is `" + `${profile[men.id].credits}$` + "`.**")
+message.channel.send(`** ${men.username}, :credit_card: balance` + " is `" + `${profile[men.id].credits}$` + "`.**")/// Mal , Codes
 } else {
   message.channel.send(`** ${message.author.username}, your :credit_card: balance` + " is `" + `${profile[message.author.id].credits}$` + "`.**")
 }
 }
  
-if(message.content.startsWith(prefix + "daily")) {
-  if(profile[message.author.id].lastDaily != moment().format('day')) {
+if(message.content.startsWith(prefix + "daily")) { /// Mal , Codes 
+  if(profile[message.author.id].lastDaily != moment().format('day')) { /// Mal , Codes 
     profile[message.author.id].lastDaily = moment().format('day')
     profile[message.author.id].credits += 200
      message.channel.send(`**${message.author.username} you collect your \`200\` :dollar: daily pounds**`)
@@ -106,70 +37,55 @@ if(message.content.startsWith(prefix + "daily")) {
   }
  
  let cont = message.content.slice(prefix.length).split(" ");
-let args = cont.slice(1);
+let args = cont.slice(1); /// Mal , Codes 
 let sender = message.author
-if(message.content.startsWith(prefix + 'trans')) {
+if(message.content.startsWith(prefix + 'trRans')) {
           if (!args[0]) {
-            message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
+            message.channel.send(`**Usage: ${prefix}trans @someone amount**`); /// Mal , Codes 
          return;
            }
-        // We should also make sure that args[0] is a number
+        // We should also make sure that args[0] is a number /// Mal , Codes 
         if (isNaN(args[0])) {
             message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
             return; // Remember to return if you are sending an error message! So the rest of the code doesn't run.
              }
             let defineduser = '';
-            let firstMentioned = message.mentions.users.first();
+            let firstMentioned = message.mentions.users.first(); /// Mal , Codes 
             defineduser = (firstMentioned)
             if (!defineduser) return message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
             var mentionned = message.mentions.users.first();
 if (!profile[sender.id]) profile[sender.id] = {}
-if (!profile[sender.id].credits) profile[sender.id].credits = 200;
+if (!profile[sender.id].credits) profile[sender.id].credits = 200; /// Mal , Codes
 fs.writeFile('profile.json', JSON.stringify(profile), (err) => {
 if (err) console.error(err);
 })
       var mando = message.mentions.users.id;
       if  (!profile[defineduser.id]) profile[defineduser.id] = {}
-      if (!profile[defineduser.id].credits) profile[defineduser.id].credits = 200;
+      if (!profile[defineduser.id].credits) profile[defineduser.id].credits = 200; /// Mal , Codes
       profile[defineduser.id].credits += (+args[0]);
       profile[sender.id].credits += (-args[0]);
-      let mariam = message.author.username
+      let mariam = message.author.username /// Mal , Codes
 message.channel.send(`**:moneybag: | ${message.author.username}, has transferrerd ` + "`" + args[0] + "$` to " + `<@${defineduser.id}>**`)
 }
- 
-      });//////////////////- Mál ,ϻά𝔥𝔪𝔬𝔲𝓓-QuaStyle≽ܫ≼ 5661
- 
+  
+      }); /// Mal , Codes 
+  
       client.on('message', message => {
-          if(!profile[message.author.id]) profile[message.author.id] ={
+          if(!profile[message.author.id]) profile[message.author.id] ={ /// Mal , Codes
               points: 0,
-              level: 1
+              level: 1 /// Mal , Codes 
           };
           if(message.author.bot) return;
-          profile[message.author.id].points = Math.floor(profile[message.author.id].points+1);
-          if(profile[message.author.id].points > 100) {
+          profile[message.author.id].points = Math.floor(profile[message.author.id].points+1); /// Mal , Codes
+          if(profile[message.author.id].points > 100) { 
               profile[message.author.id].points = 0
               profile[message.author.id].level = Math.floor(profile[message.author.id].level+1);
-              message.channel.send(`**${message.author.username}, You leveld up to __${profile[message.author.id].level}__**`)//////////////////- Mál ,Team
+              message.channel.send(`**${message.author.username}, You leveld up to __${profile[message.author.id].level}__**`)
           }
           fs.writeFile('profile.json', JSON.stringify(profile), (err) => {
 if (err) console.error(err);
-})
-      })//////////////////- Mál ,ϻά𝔥𝔪𝔬𝔲𝓓-QuaStyle≽ܫ≼ 5661
- 
-    client.on('message', message => {
-        let tit = message.content.split(" ").slice(1).join(" ");
-        if(message.content.startsWith(prefix + "title")) {
-        if(!profile[message.author.id].tite) profile[message.author.id].tite = "Hey im using Super"
-        if(!tit) {
-            message.channel.send("**Usage: <title <something>**");
-        } else {
-            profile[message.author.id].tite = tit
-            message.channel.send(`:ok:`)
-        }
-        }
-        fs.writeFile('profile.json', JSON.stringify(profile), (err) => {
-if (err) console.error(err);
-})
-    })//////////////////- Mál ,ϻά𝔥𝔪𝔬𝔲𝓓-QuaStyle≽ܫ≼ 5661
+})  /// Mal , Codes 
+      }) /// Mal , Codes 
+     
 
 client.login(process.env.BOT_TOKEN);
